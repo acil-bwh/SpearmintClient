4c5b7b8dca93dc57646caef2c649b3ac19ca4379
atisman89 <jinho89@gmail.com>
2015-08-09 23:33:16 -0400

    Initial commit

---
+++ experiment.py
@@ -0,0 +1,53 @@
+import json
+import requests
+
+API_URL = 'http://ec2-52-11-215-50.us-west-2.compute.amazonaws.com/api'
+#API_URL = 'http://localhost:8000/api'
+
+class Experiment:
+    def __init__(self,
+                 name,
+                 description='',
+                 parameters=None,
+                 outcome=None,
+                 access_token=None,
+                 likelihood='GAUSSIAN'): # other option is NOISELESS
+        self.name = name
+        self.parameters = parameters
+        self.outcome = outcome
+        self.access_token = access_token
+        api_params = {'name': name}
+        r = self.call_api('find_experiment', method='get', params=api_params)
+        if (r['result']): # found experiment
+            print 'resuming experiment ' + name + '...'
+        else:
+            api_params['parameters'] = parameters
+            api_params['outcome'] = outcome
+            r = self.call_api('create_experiment', method='post', params=api_params)
+            if 'error' in r:
+                raise RuntimeError('failed to create experiment ' + name + '. error: ' + r['error'])
+            else:
+                print 'experiment ' + name + ' was created.'
+
+    def call_api(self, name, method, params):
+        url = '{0}/{1}/'.format(API_URL, name)
+        headers = {'Authorization': 'Bearer ' + self.access_token}
+        if method.lower() == 'post':
+            r = requests.post(url, headers=headers, data=json.dumps(params))
+        elif method.lower() == 'get':
+            r = requests.get(url, headers=headers, params=params)
+        return r.json()
+
+    def suggest(self):
+        api_params = {'name': self.name}
+        r = self.call_api('get_suggestion', method='get', params=api_params)
+        if 'error' in r:
+            raise RuntimeError('failed to get suggestion from spearmint. error: ' + r['error'])
+        else:
+            return r['params']
+
+    def update(self, param_values, outcome_val):
+        api_params = {'name': self.name, 'outcome_val': outcome_val}
+        r = self.call_api('post_update', method='post', params=api_params)
+        if 'error' in r:
+            raise RuntimeError('failed to post update to spearmint. error: ' + r['error'])
---
+++ tune_branin_example.py
@@ -0,0 +1,36 @@
+import numpy as np
+import math
+import SpearmintClient
+
+def branin(x, y):
+
+    result = np.square(y - (5.1/(4*np.square(math.pi)))*np.square(x) +
+         (5/math.pi)*x - 6) + 10*(1-(1./(8*math.pi)))*np.cos(x) + 10
+
+    result = float(result)
+
+    print 'Result = %f' % result
+    #time.sleep(np.random.randint(60))
+    return result
+
+
+parameters = {'x':{'min':-5, 'max':10, 'type':'float'},
+              'y':{'min':0, 'max':15, 'type':'float'}
+             }
+outcome = {'name':'Function_Value', 'minimize':True} # additional outcome parameter 'minimize'
+#access_token = 'uykCmyN8y0gUxYiTkBDt8lr9QdDnQr'
+access_token = 'dsv9crwrv87UEroGVw1F1PFD3muNjp'
+
+scientist = SpearmintClient.Experiment(name="branin_tuner",
+                                       description="Tuning a simple Spearmint example",
+                                       parameters=parameters,
+                                       outcome=outcome,
+                                       access_token=access_token)
+
+for i in range(5):
+    # Get a hyperparameter suggestion from Whetlab
+    params = scientist.suggest()
+
+    score = 1.0 * branin(params['x'], params['y'])
+
+    scientist.update(params, score)
