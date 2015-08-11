d5d69d014b89ea40424701227fb43194e37050e4
atisman89 <jinho89@gmail.com>
2015-08-10 22:37:34 -0400

    Ade setup script

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='SpearmintClient',
+      description='Light-weight client API to the Spearmint Web server',
+      version='0.9.0',
+      author='J. Lee',
+      author_email='jinho89@gmail.com',
+      packages=['SpearmintClient'],
+     )
